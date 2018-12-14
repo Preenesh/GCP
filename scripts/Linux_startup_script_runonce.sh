72ed5286617c80f25bffe926876754fee9307292
Preenesh <preenesh@gmail.com>
2018-12-14 18:17:34 +0530

    Fixed Linux_startup_script_runonce

--- scripts/Linux_startup_script_runonce.sh
+++ scripts/Linux_startup_script_runonce.sh
@@ -28,12 +28,34 @@ get_metadata_value(){
 	echo $metadata_value
 }
 
+# Get Instance Name and Zone from Metadata
+instance_name_url='instance/name'
+instance_zone_url='instance/zone'
 
-# Instance Name
-'instance/name'
+instance_name=$(get_metadata_value $instance_name_url)
+instance_zone=$(get_metadata_value $instance_zone_url | cut -d/ -f4)
 
-# Add instance metadata
+# Check if Custom Metadata run-startup-script exist and set it if it does not with run-startup-script value as "yes"
+run_script_metadata_url='instance/attributes/run-startup-script'
+run_script_metadata_response=$(get_metadata_response $run_script_metadata_url)
+if [ "$run_script_metadata_response" -eq 404 ]; # Custom Metadata run-startup-script does not exist
+then
+gcloud compute instances add-metadata $instance_name --zone $instance_zone --metadata=run-startup-script=yes --quiet
+fi
 
-VMNAME=$(curl -H Metadata-Flavor:Google http://metadata/computeMetadata/v1/instance/hostname | cut -d. -f1)
-ZONE=$(curl -H Metadata-Flavor:Google http://metadata/computeMetadata/v1/instance/zone | cut -d/ -f4)
-gcloud compute instances add-metadata $VMNAME --zone $ZONE --metadata=test1=yes --quiet
+# Check if Custom Metadata run-startup-script value is set to "yes" and set it to "no" once execution is completed
+run_script_metadata_value=$(get_metadata_value $run_script_metadata_url)
+if [ "$run_script_metadata_value" = "yes" ];
+then
+
+# --------------------- START Startup-Script ------------------------
+
+apt-get update
+apt-get install -y apache2
+
+# --------------------- END Startup-Script ---------------------------
+
+gcloud compute instances add-metadata $instance_name --zone $instance_zone --metadata=run-startup-script=no --quiet
+else
+ echo "Skipping Startup-Script as Custom Metadata run-startup-script is set to no"
+fi
